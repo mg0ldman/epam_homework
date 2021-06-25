73d12b70df64e6d2f2a1b03c1286f486e2f8dfba
g0ldman <html.zzz@outlook.com>
2021-06-25 16:32:41 +0500

    add files for HW1

---
+++ homework1/task02.py
@@ -0,0 +1,17 @@
+"""
+Given a cell with "it's a fib sequence" from slideshow,
+please write function "check_fib", which accepts a Sequence of integers, and
+returns if the given sequence is a Fibonacci sequence
+We guarantee, that the given sequence contain >= 0 integers inside.
+"""
+from typing import Sequence
+
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    """The function checks if a given sequence is a Fibonacci one"""
+    if len(data) < 3 or (data[0] != 0 or data[1] != 1):
+        return False
+    for i in range(len(data) - 2):
+        if data[i] + data[i + 1] != data[i + 2]:
+            return False
+    return True
---
+++ tests/homework1/test_task05.py
@@ -0,0 +1,15 @@
+import pytest
+
+from homework1.task05 import find_maximal_subarray_sum
+
+
+def test_non_empty_list_case():
+    """Testing that the function returns a correct result
+    with a nonempty list"""
+    assert find_maximal_subarray_sum([1, 3, -1, -3, 5, 3, 6, 7], 3) == 16
+
+
+def test_empty_list_case():
+    """Testing that function returns a correct result
+    when list's length is zero"""
+    assert find_maximal_subarray_sum([], 0) == 0
