75e16d58330db7b0410491b104a4499dafab0476
Ilya Vlasov <html.zzz@outlook.com>
2021-07-10 14:34:19 +0500

    add files for HW4

--- tests/homework4/test_task_3_get_print_output.py
+++ tests/homework4/test_task_3_get_print_output.py
@@ -2,6 +2,8 @@ from homework4.task_3_get_print_output import my_precious_logger
 
 
 def test_my_precious_logger_stderr(capsys):
+    """Testing that function writes string to stderr if
+    it starts with an "error" substring"""
     my_precious_logger('error: file not found')
     captured = capsys.readouterr()
     assert captured.err == 'error: file not found'
@@ -9,6 +11,8 @@ def test_my_precious_logger_stderr(capsys):
 
 
 def test_my_precious_logger_stdout(capsys):
+    """Testing that function writes string to stdout if
+    it doesn't start with an "error" substring"""
     my_precious_logger('abc')
     captured = capsys.readouterr()
     assert captured.out == 'abc'
@@ -16,6 +20,8 @@ def test_my_precious_logger_stdout(capsys):
 
 
 def test_my_precious_logger_empty_str(capsys):
+    """Testing that stderr and stdout remain empty if
+     the string passed to the function is empty"""
     my_precious_logger('')
     captured = capsys.readouterr()
     assert captured.out == ''
