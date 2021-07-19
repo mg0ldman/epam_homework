9b65d0cbdbc623b5e9cfd0cd086d384538e3e5a4
Ilya Vlasov <html.zzz@outlook.com>
2021-07-19 09:27:35 +0500

    correct mistakes in the 1st task HW3

--- homework3/task01.py
+++ homework3/task01.py
@@ -6,7 +6,7 @@ from typing import Callable
 def cache(times: int) -> Callable:
     """The function returns a cached result of the argument-function
      for a given number of times"""
-    cache_storage = {}
+    cache_storage = []
     cnt = 0
     if times < 0:
         raise ValueError('"times" argument should be greater '
@@ -16,12 +16,13 @@ def cache(times: int) -> Callable:
         def cached_func(*args):
             nonlocal cnt
             if cnt == 0:
+                cache_storage.clear()
                 cnt += 1
-                cache_storage[args] = func(*args)
+                cache_storage.append((func, func(*args)))
             if times >= cnt > 0:
                 cnt += 1
             else:
                 cnt = 0
-            return cache_storage[args]
+            return cache_storage[0]
         return cached_func
     return wrapper
